298db371c14ddeb58a4a36f30ca1e98a003249fe
Jakob Willforss <jakob.willforss@hotmail.com>
2018-01-20 10:44:39 +0100

    JSON link to country code in place. Now I should be able to color countries based on the generated values.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ d3.json('data/world.json', function (err, data) {
         .style("opacity", 0).remove();
 
     var countries = topojson.feature(data, data.objects.countries);
+
+    // Setup the canvas
     var canvas = d3.select("body").append("canvas")
         .attr({width: "1024px", height: "512px"});
 
@@ -19,34 +21,51 @@ d3.json('data/world.json', function (err, data) {
     var path = d3.geo.path()
         .projection(projection).context(context);
 
-    context.strokeStyle = "#333";
+    // Setup stroke style
+    context.strokeStyle = "#990";
     context.lineWidth = 0.25;
-    context.fillStyle = "#fff";
+    context.fillStyle = "#050";
+
+    // Perform path drawing
     context.beginPath();
     path(countries);
+
+    // Fill countries with white content
     context.fill();
+
+    // Draw lines between countries
     context.stroke();
-    console.log(canvas.node().toDataURL());
+    // console.log(canvas.node().toDataURL());
 
-    // Next step in the tutorial
+    // // Next step in the tutorial
 
+    // Setup Three scene
     var scene = new THREE.Scene();
 
+    // Setup camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 6000);
     camera.position.z = 500;
 
+    // Setup THREE renderer
     var renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    // Add light
     var light = new THREE.HemisphereLight('#fff', '#666', 1.5);
     light.position.set(0, 500, 0);
     scene.add(light);
 
-    var waterMaterial = new THREE.MeshBasicMaterial({color: '#555', transparent: true});
+    // Generate material (and color) for base globe
+    var waterMaterial = new THREE.MeshBasicMaterial({color: '#003', transparent: true});
+
+    // Setup a geometry
     var sphere = new THREE.SphereGeometry(200, 100, 100);
+
+    // Generate a mesh layer based on materials and geometry
     var baseLayer = new THREE.Mesh(sphere, waterMaterial);
 
+    // Generate a texture
     var mapTexture = new THREE.Texture(canvas.node());
     mapTexture.needsUpdate = true;
 
@@ -61,7 +80,7 @@ d3.json('data/world.json', function (err, data) {
     function render() {
         root.rotation.y += 0.02;
         requestAnimationFrame(render);
-        render.render(scene, camera);
+        renderer.render(scene, camera);
     }
 
     render();
